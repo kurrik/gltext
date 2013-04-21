920f26836cf72ddd9bd1e0875933cb42bb294349
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-04-21 02:02:25 -0700

    Remove dependencies on glh

--- font.go
+++ font.go
@@ -7,7 +7,6 @@ package gltext
 import (
 	"fmt"
 	"github.com/go-gl/gl"
-	"github.com/go-gl/glh"
 	"image"
 )
 
@@ -19,6 +18,100 @@ type Font struct {
 	maxGlyphWidth  int         // Largest glyph width.
 	maxGlyphHeight int         // Largest glyph height.
 }
+
+// CheckGLError returns an opengl error if one exists.
+func CheckGLError() error {
+	errno := gl.GetError()
+
+	if errno == gl.NO_ERROR {
+		return nil
+	}
+	return fmt.Errorf("Unknown GL error: %d", errno)
+}
+
+// Pow2 returns the first power-of-two value >= to n.
+// This can be used to create suitable texture dimensions.
+func Pow2(x uint32) uint32 {
+	x--
+	x |= x >> 1
+	x |= x >> 2
+	x |= x >> 4
+	x |= x >> 8
+	x |= x >> 16
+	return x + 1
+}
+
+// IsPow2 returns true if the given value is a power-of-two.
+func IsPow2(x uint32) bool { return (x & (x - 1)) == 0 }
+
+// Why the image.Image interface does not support this,
+// I can never understand.
+type copyable interface {
+	image.Image
+	Set(x, y int, clr color.Color)
+}
+
+func copyImg(src, dst copyable) image.Image {
+	var x, y int
+	sb := src.Bounds()
+
+	for y = 0; y < sb.Dy(); y++ {
+		for x = 0; x < sb.Dx(); x++ {
+			dst.Set(x, y, src.At(x, y))
+		}
+	}
+
+	return dst
+}
+
+// Pow2Image returns the given image, scaled to the smallest power-of-two
+// dimensions larger or equal to the input dimensions.
+// It preserves the image format and contents.
+//
+// This is useful if an image is to be used as an OpenGL texture.
+// These often require image data to have power-of-two dimensions.
+func Pow2Image(src image.Image) image.Image {
+	sb := src.Bounds()
+	w, h := uint32(sb.Dx()), uint32(sb.Dy())
+
+	if IsPow2(w) && IsPow2(h) {
+		return src // Nothing to do.
+	}
+
+	rect := image.Rect(0, 0, int(Pow2(w)), int(Pow2(h)))
+
+	switch src := src.(type) {
+	case *image.Alpha:
+		return copyImg(src, image.NewAlpha(rect))
+
+	case *image.Alpha16:
+		return copyImg(src, image.NewAlpha16(rect))
+
+	case *image.Gray:
+		return copyImg(src, image.NewGray(rect))
+
+	case *image.Gray16:
+		return copyImg(src, image.NewGray16(rect))
+
+	case *image.NRGBA:
+		return copyImg(src, image.NewNRGBA(rect))
+
+	case *image.NRGBA64:
+		return copyImg(src, image.NewNRGBA64(rect))
+
+	case *image.Paletted:
+		return copyImg(src, image.NewPaletted(rect, src.Palette))
+
+	case *image.RGBA:
+		return copyImg(src, image.NewRGBA(rect))
+
+	case *image.RGBA64:
+		return copyImg(src, image.NewRGBA64(rect))
+	}
+
+	panic(fmt.Sprintf("Unsupported image format: %T", src))
+}
+
 
 // loadFont loads the given font data. This does not deal with font scaling.
 // Scaling should be handled by the independent Bitmap/Truetype loaders.
@@ -32,7 +125,7 @@ func loadFont(img *image.RGBA, config *FontConfig) (f *Font, err error) {
 	f.config = config
 
 	// Resize image to next power-of-two.
-	img = glh.Pow2Image(img).(*image.RGBA)
+	img = Pow2Image(img).(*image.RGBA)
 	ib := img.Bounds()
 
 	// Create the texture itself. It will contain all glyphs.
@@ -100,7 +193,7 @@ func loadFont(img *image.RGBA, config *FontConfig) (f *Font, err error) {
 		gl.EndList()
 	}
 
-	err = glh.CheckGLError()
+	err = CheckGLError()
 	return
 }
 
@@ -246,7 +339,7 @@ func (f *Font) Printf(x, y float32, fs string, argv ...interface{}) error {
 	gl.MatrixMode(gl.PROJECTION)
 	gl.PopMatrix()
 	gl.PopAttrib()
-	return glh.CheckGLError()
+	return CheckGLError()
 }
 
 // GlyphBounds returns the largest width and height for any of the glyphs
